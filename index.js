837f4f0eb1c78f7248d57e2ccde44605d550f3f6
Daniel Schneider <daschne8@gmail.com>
2019-06-10 16:30:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const var2 = 31
 const multiply = var1 * var2
 const mod = 12 % 8
 const random = Math.floor(Math.random() * 10) +1
-const max = Math.max(1,2,3,4,5)
+const max = Math.max(1,2,3,4,5,20)
